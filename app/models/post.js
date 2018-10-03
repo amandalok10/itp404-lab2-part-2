8338562f2eb976d31dea762b94e5a04e86366aa5
amanda <loka@usc.edu>
2018-10-02 21:31:54 -0700

    update

--- app/models/post.js
+++ app/models/post.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
+  first: DS.attr('string'),
   last: DS.attr('string'),
   jobTitle: DS.attr('string'),
 phone: DS.attr('string'),
